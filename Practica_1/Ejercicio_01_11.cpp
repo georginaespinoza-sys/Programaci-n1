3a6afb4a7762402078609db1ed7c36d19a162c9f
georginaespinoza-sys <georgina.espinoza@ucb.edu.bo>
2025-08-26 16:04:53 -0400

    termine el 11

--- Practica_1/Ejercicio_01_11.cpp
+++ Practica_1/Ejercicio_01_11.cpp
@@ -4,3 +4,13 @@
 // Carrera del estudiante: Ingeniería de Mecatrónica
 // Fecha creación: 25/08/2025
 // Número de ejercicio: 11
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+    return 0;
+}
