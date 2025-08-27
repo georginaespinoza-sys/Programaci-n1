bfc93cd873cf865008a7a4f3f3eb1b8a701e9646
georginaespinoza-sys <georgina.espinoza@ucb.edu.bo>
2025-08-27 18:55:42 -0400

    ejercicio 11

--- Practica_1/Ejercicio_01_11.cpp
+++ Practica_1/Ejercicio_01_11.cpp
@@ -2,15 +2,74 @@
 // Autor: Georgina Mishel Espinoza Mamani
 // Carnet: 9979732 L.P.
 // Carrera del estudiante: Ingeniería de Mecatrónica
-// Fecha creación: 25/08/2025
+// Fecha creación: 26/08/2025
 // Número de ejercicio: 11
 
 #include <iostream>
 
 using namespace std;
 
-int main(int argc, char const *argv[])
+
+int main()
 {
-    /* code */
+    int mes;
+    cout<<"Introduce el numero de un mes: "<<endl;
+    cin>>mes;
+    if (mes<0)
+    {
+        cout<<"Debe ser mayor a 0";
+    }
+    else if (mes>0)
+    {
+        if(mes==1)
+        {
+            cout<<"enero"<<endl;
+        }
+        else if(mes==2)
+        {
+            cout<<"febrero"<<endl;
+        }
+        else if(mes==3)
+        {
+            cout<<"marzo"<<endl;
+        }
+         else if(mes==4)
+        {
+            cout<<"abril"<<endl;
+        }
+         else if(mes==5)
+        {
+            cout<<"mayo"<<endl;
+        }
+         else if(mes==6)
+        {
+            cout<<"junio"<<endl;
+        }
+         else if(mes==7)
+        {
+            cout<<"julio"<<endl;
+        }
+         else if(mes==8)
+        {
+            cout<<"agosto"<<endl;
+        }
+         else if(mes==9)
+        {
+            cout<<"septiembre"<<endl;
+        }
+         else if(mes==10)
+        {
+            cout<<"octubre"<<endl;
+        }
+         else if(mes==11)
+        {
+            cout<<"noviembre"<<endl;
+        }
+         else if(mes==12)
+        {
+            cout<<"diciembre"<<endl;
+        }
+    }
+    
     return 0;
 }
