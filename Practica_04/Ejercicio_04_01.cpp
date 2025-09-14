8337d9730ac63154b9b47ba2651ff56e18210b2a
georginaespinoza-sys <georgina.espinoza@ucb.edu.bo>
2025-09-14 13:45:03 -0400

    practica 4

---
+++ Practica_04/Ejercicio_04_01.cpp
@@ -0,0 +1,34 @@
+// Materia: Programación I, Paralelo 4
+// Autor: Georgina Mishel Espinoza Mamani
+// Carnet: 9979732 L.P.
+// Carrera del estudiante: Ingeniería de Mecatrónica
+// Fecha creación: 22/08/2025
+// Número de ejercicio: 1
+
+#include <iostream>
+using namespace std;
+
+void intercambiarValores(int& num1, int& num2);
+
+int main() {
+    int num1 = 20, num2 = 11;
+
+    cout << "El valor de num1 es: " << num1 <<endl;
+    cout << "El valor de num2 es: " << num2 <<endl;
+    
+    intercambiarValores(num1, num2);//para intercambiar los valores
+    
+    cout << "El nuevo valor de num1 es: " << num1 <<endl;
+    cout << "El nuevo valor de num2 es: " << num2 <<endl;
+
+    return 0;
+}
+
+void intercambiarValores(int& num1, int& num2) {//intercambiar valores por referencia
+    int aux; //para el intercambio
+    
+    // Proceso de intercambio
+    aux = num1;
+    num1 = num2;
+    num2 = aux;
+}
---
+++ Practica_04/Ejercicio_04_05.cpp
@@ -0,0 +1,6 @@
+// Materia: Programación I, Paralelo 4
+// Autor: Georgina Mishel Espinoza Mamani
+// Carnet: 9979732 L.P.
+// Carrera del estudiante: Ingeniería de Mecatrónica
+// Fecha creación: 22/08/2025
+// Número de ejercicio: 5
