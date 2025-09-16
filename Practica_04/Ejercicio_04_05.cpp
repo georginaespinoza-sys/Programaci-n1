5aa7088919c896da87837c7a6408156f762bee8b
georginaespinoza-sys <georgina.espinoza@ucb.edu.bo>
2025-09-16 12:16:30 -0400

    Ejercicio 5

--- Practica_04/Ejercicio_04_05.cpp
+++ Practica_04/Ejercicio_04_05.cpp
@@ -2,5 +2,47 @@
 // Autor: Georgina Mishel Espinoza Mamani
 // Carnet: 9979732 L.P.
 // Carrera del estudiante: Ingeniería de Mecatrónica
-// Fecha creación: 22/08/2025
+// Fecha creación: 16/09/2025
 // Número de ejercicio: 5
+
+#include <iostream>
+using namespace std;
+
+void calcularSalario(int horasTrabajadas, float tarifaPorHora, float bonoPorHoraExtra) {
+    float salario= 0.0;
+    float salarioBonificacion = 0.0;
+    int horasExtra = 0;
+
+    if (horasTrabajadas > 8) {
+        horasExtra=horasTrabajadas-8;
+        salario=8*tarifaPorHora;
+        salarioBonificacion = horasExtra * bonoPorHoraExtra;
+    } else {
+        salario=horasTrabajadas * tarifaPorHora;
+    }
+    
+    float salarioTotal = salario+salarioBonificacion;
+
+    cout << "Salario regular: " << salario<< endl;
+    cout << "Salario con bonificacion: " << salarioBonificacion << endl;
+    cout << "Salario total del trabajador: " << salarioTotal << endl;
+}
+
+int main() {
+    int horasTrabajadas;
+    float tarifaPorHora;
+    float bonoPorHoraExtra;
+
+    cout << "Ingrese las horas trabajadas: ";
+    cin >> horasTrabajadas;
+
+    cout << "Ingrese la tarifa por hora: ";
+    cin >> tarifaPorHora;
+
+    cout << "Ingrese el valor de la bonificacion por hora extra: ";
+    cin >> bonoPorHoraExtra;
+
+    calcularSalario(horasTrabajadas, tarifaPorHora, bonoPorHoraExtra);//función para calcular 
+
+    return 0;
+}
