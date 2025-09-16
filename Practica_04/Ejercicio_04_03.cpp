40fdc7391e3c8bc6887a6b56cd5629cfcb84f0a4
georginaespinoza-sys <georgina.espinoza@ucb.edu.bo>
2025-09-16 09:57:35 -0400

    Ejercicio 3 y 4

--- Practica_04/Ejercicio_04_03.cpp
+++ Practica_04/Ejercicio_04_03.cpp
@@ -2,5 +2,30 @@
 // Autor: Georgina Mishel Espinoza Mamani
 // Carnet: 9979732 L.P.
 // Carrera del estudiante: Ingeniería de Mecatrónica
-// Fecha creación: 22/08/2025
+// Fecha creación: 16/09/2025
 // Número de ejercicio: 3
+
+#include <iostream>
+using namespace std;
+
+void ModificarValores(int valor, int &referencia) {
+    valor = valor * 2;
+    cout << "El valor pasado por valor es: " << valor <<endl;
+
+    referencia = referencia + 10;//modificacion por referencia
+    cout << "El valor pasado por referencia es: " << referencia <<endl;
+}
+
+int main() {
+    
+    int num1, num2;
+
+    cout <<"Ingrese un valor para el primer numero: ";
+    cin >> num1;
+    cout <<"Ingrese un valor para el segundo numero: ";
+    cin >> num2;
+
+    ModificarValores(num1, num2);//llamamos a la funcion
+    
+    return 0;
+}
--- Practica_04/Ejercicio_04_04.cpp
+++ Practica_04/Ejercicio_04_04.cpp
@@ -2,5 +2,45 @@
 // Autor: Georgina Mishel Espinoza Mamani
 // Carnet: 9979732 L.P.
 // Carrera del estudiante: Ingeniería de Mecatrónica
-// Fecha creación: 22/08/2025
+// Fecha creación: 16/09/2025
 // Número de ejercicio: 4
+
+#include <iostream>
+using namespace std;
+
+float ConvertirCelsiusAFahrenheit(float celsius) {
+    float fahrenheit=(celsius*1.8)+32;
+    return fahrenheit;
+}
+
+float MayorTemperatura(float temperatura1, float temperatura2){
+    
+    if(temperatura1>temperatura2){
+        return temperatura1;
+    }
+    else{
+        return temperatura2;
+    } 
+}
+
+int main()
+{
+    float temperatura_celsius;
+    float temperatura1, temperatura2;
+
+    cout << "Introduce la temperatura en Celsius: ";
+    cin >> temperatura_celsius;
+
+    float temperatura_fahrenheit = ConvertirCelsiusAFahrenheit(temperatura_celsius);//funcion "ConvertirCelsiusAFahrenheit" con el valor de entrada
+    cout << "La conversion es: " << temperatura_fahrenheit << "Fahrenheit" << endl;
+
+    cout << "Introduce la primera temperatura: ";
+    cin >> temperatura1;
+    cout << "Introduce la segunda temperatura: ";
+    cin >> temperatura2;
+
+    float mayor_temperatura = MayorTemperatura(temperatura1, temperatura2);//funcion "MayorTemperatura" con los dos valores
+    cout << "La mayor temperatura es: " << mayor_temperatura << endl;
+
+    return 0;
+}
