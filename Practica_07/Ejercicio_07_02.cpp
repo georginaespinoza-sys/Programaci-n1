2d86ec189bc133712f95c9584881a599c23b605b
georginaespinoza-sys <georgina.espinoza@ucb.edu.bo>
2025-10-15 09:08:26 -0400

    Ejercicio 2

--- Practica_07/Ejercicio_07_02.cpp
+++ Practica_07/Ejercicio_07_02.cpp
@@ -3,7 +3,7 @@ Materia: Programación I, Paralelo 4
 Autor: Georgina Mishel Espinoza Mamani
 Carnet: 9979732 L.P.
 Carrera: Ingenieria Mecatrónica
-Fecha creación: 12/10/2025
+Fecha creación: 14/10/2025
 Número de ejercicio: 2
 Problema planteado: Generar una matriz de N x N con números al azar entre A y B, y determinar: 
 • La suma de la última columna 
@@ -15,6 +15,7 @@ Problema planteado: Generar una matriz de N x N con números al azar entre A y B
 #include <iostream>
 #include <ctime>
 #include <cstdlib>
+#include <cmath>
 
 using namespace std;
 
@@ -22,42 +23,70 @@ int main()
 {
     srand(time(NULL));
 
-    int fila, columna;
     int matriz[50][50];
     int suma = 0;
     int producto = 1;
 
-    cout<<"Ingrese la fila";
-    cin>>fila;
-    cout<<"Ingrese la columna";
-    cin>>columna;
+    int N;
+    cout << "Ingrese el tamanio de la matriz (N x N): ";
+    cin >> N;
+
     
     //Llenar la matriz
-    for(int i=0 ; i<fila ; i++){
-        for(int j=0 ; j<columna ; j++){
+    for(int i=0 ; i<N ; i++){
+        for(int j=0 ; j<N ; j++){
             matriz[i][j] = rand()%50;
         }
     }
 
     //Mostrar la matriz
-    for(int i=0 ; i<fila ; i++){
-        for(int j=0 ; j<columna ; j++){
+    for(int i=0 ; i<N ; i++){
+        for(int j=0 ; j<N ; j++){
             cout<<matriz[i][j]<<"\t";
         }
         cout<<endl;
     }
 
     //Sumar la ultima columna
-    for(int i=0 ; i<fila ; i++){
-        suma += matriz[i][columna-1];
+    for(int i=0 ; i<N ; i++){
+        suma += matriz[i][N-1];
     }
     cout<<"La suma es: "<<suma<<endl;
 
     //Producto de la ultila fila
-    for(int j=0 ; j<columna ; j++){
-        producto *= matriz[fila-1][j];
+    for(int j=0 ; j<N ; j++){
+        producto *= matriz[N-1][j];
     }
-    cout<<"El producto es: "<<producto;
+    cout<<"El producto es: "<<producto<<endl;
+
+
+    //Mayor valor
+    int mayor = matriz[0][0];
+    int filaMayor = 0, columnaMayor = 0;
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            if (matriz[i][j] > mayor) {
+                mayor = matriz[i][j];
+                filaMayor = i;
+                columnaMayor = j;
+            }
+        }
+    }
+    cout << "Mayor valor: " << mayor << ", posicion: (" << filaMayor << ", " << columnaMayor << ")" << endl;
+
+
+    //Desviacion estandar
+    double sumaTotal = 0, sumaCuadrados = 0;
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            sumaTotal += matriz[i][j];
+            sumaCuadrados += matriz[i][j] * matriz[i][j];
+        }
+    }
+    double media = sumaTotal / (N * N);
+    double desviacion = sqrt( (sumaCuadrados / (N * N)) - (media * media) );
+    cout << "Desviacion estandar: " << desviacion << endl;
+
 
 
     return 0;
